9698c8d0b3b5f5b494e772fc8915a25b74ab31d3
Navin Chandra <nc01009@surrey.ac.uk>
2025-03-30 22:42:30 +0100

    another two more approaches

--- Blind75/anagram.py
+++ Blind75/anagram.py
@@ -26,6 +26,24 @@ class Solution:
             # return hashmap_s == hashmap_t
 
         return True
+    
+
+    def isAnagram_2(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+        
+        hashmap_s,  hashmap_t = {}, {}
+
+        for i in range(len(s)):
+            hashmap_s[s[i]] = 1 + hashmap_s.get(s[i],0)
+            hashmap_t[t[i]] = 1 + hashmap_t.get(t[i],0)
+
+        for char in hashmap_s:
+            if hashmap_s[char] != hashmap_t.get(char, 0):
+                return False
+
+        return True
 
 
 program = Solution()
@@ -34,6 +52,6 @@ program = Solution()
 s = "rat"
 t = "car"
 
-print(program.isAnagram(s=s,t=t))
+print(program.isAnagram_2(s=s,t=t))
 
 
