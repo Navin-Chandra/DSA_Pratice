5aa07029971d66e5f5dcea8d446ac08e1ce9ead7
Navin Chandra <nc01009@surrey.ac.uk>
2025-04-04 00:01:40 +0100

    group anagram code using hash

---
+++ Blind75/group_anagram.py
@@ -0,0 +1,26 @@
+
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res =   defaultdict(list)
+
+        for s in strs:
+            count =[0]*26 # a ...... z for all letter 0 count initially
+
+            for c in s:
+                # count[ord(s) - ord("a")] +=1
+                count[ord(c) - ord("a")] += 1
+
+
+            res[tuple(count)].append(s)
+        
+        print(res)
+
+        return res.values()
+
+program = Solution()
+strs =["eat","tea","tan","ate","nat","bat"]
+
+print(program.groupAnagrams(strs=strs))
